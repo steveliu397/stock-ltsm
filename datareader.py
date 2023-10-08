99599a2817d3be715f929305ac9704e71cc42f0b
Steve Liu <steveliu397@Steves-Air.lan>
2023-10-08 02:35:00 -0400

    updated datareader

--- datareader.py
+++ datareader.py
@@ -8,46 +8,40 @@ import numpy as np
 # import tensorflow as tf # This code has been tested with TensorFlow 1.6
 from sklearn.preprocessing import MinMaxScaler
 
+DATA_SOURCE = "alphavantage"
+API_KEY = '8NR1BD7HMN9AYF44'
+
 class DataReader:
     ticker = "AAL"
+    file_to_save = ''
 
-    def __init__(self, ticker):
+    def __init__(self, ticker, file_to_save):
         self.ticker = ticker
+        self.file_to_save = file_to_save
 
     def get_data(self):
         # use API as default method
-        data_source = "alphavantage"
-        api_key = '8NR1BD7HMN9AYF44'
-
-        if data_source == "alphavantage":
+        if DATA_SOURCE == "alphavantage":
             # configures URL and name of CSV file
-            url_string = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=%s&outputsize=full&apikey=%s"%(self.ticker,api_key)
-            file_to_save = 'stock_market_data-%s.csv'%self.ticker
+            url_string = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=%s&outputsize=full&apikey=%s"%(self.ticker,API_KEY)
             
-            if not os.path.exists(file_to_save):
-                # load from URL
-                with urllib.request.urlopen(url_string) as url:
-                    data = json.loads(url.read().decode())
-                    # extract stock market data
-                    data = data['Time Series (Daily)']
-                    df = pd.DataFrame(columns=['Date','Low','High','Close','Open'])
-                    for k,v in data.items():
-                        date = dt.datetime.strptime(k, '%Y-%m-%d')
-                        data_row = [date.date(),float(v['3. low']),float(v['2. high']),
-                                    float(v['4. close']),float(v['1. open'])]
-                        df.loc[-1,:] = data_row
-                        df.index = df.index + 1
-                print('Data saved to : %s'%file_to_save)        
-                df.to_csv(file_to_save)
+            with urllib.request.urlopen(url_string) as url:
+                data = json.loads(url.read().decode())
+                # extract stock market data
+                data = data['Time Series (Daily)']
+                df = pd.DataFrame(columns=['Date','Low','High','Close','Open'])
+                for k,v in data.items():
+                    date = dt.datetime.strptime(k, '%Y-%m-%d')
+                    data_row = [date.date(),float(v['3. low']),float(v['2. high']),
+                                float(v['4. close']),float(v['1. open'])]
+                    df.loc[-1,:] = data_row
+                    df.index = df.index + 1
+            print('Data saved to : %s'%self.file_to_save)        
+            df.to_csv(self.file_to_save)
 
-            else:
-                print('File already exists. Loading data from CSV')
-                df = pd.read_csv(file_to_save)
-
+        # move this to starter
         else:
-            # read directly from saved files in the Stocks folder
-            df = pd.read_csv(os.path.join('Stocks','hpq.us.txt'),delimiter=',',usecols=['Date','Open','High','Low','Close'])
-            print('Loaded data from the Kaggle repository')
+            raise Exception("")
 
 
 
--- starter.py
+++ starter.py
@@ -13,10 +13,12 @@ from datareader import DataReader
 # outsourced data-builidng to datareader.py
 
 ticker = 'AAL'
+file_to_save = 'stock_market_data-%s.csv'%ticker
 
-my_datareader = DataReader(ticker)
+data_reader = DataReader(ticker, file_to_save)
+data_reader.get_data()
 
-df = my_datareader.sort_values('Date')
+df = pd.read_csv(file_to_save).sort_values('Date')
 
 df.head()
 
@@ -24,7 +26,28 @@ high_prices = df.loc[:,'High'].as_matrix()
 low_prices = df.loc[:,'Low'].as_matrix()
 mid_prices = (high_prices+low_prices)/2.0
 
-train_data = mid_prices[:len(mid_prices) * 0.9]
-test_data = mid_prices[len(mid_prices) * 0.1:]
+train_data = mid_prices[:15000]
+test_data = mid_prices[15000:]
 
+# check size of the dataset
 print(len(mid_prices))
+
+scaler = MinMaxScaler()
+train_data = train_data.reshape(-1,1)
+test_data = test_data.reshape(-1,1)
+
+# Train the Scaler with training data and smooth data
+smoothing_window_size = 2500
+for di in range(0,10000,smoothing_window_size):
+    scaler.fit(train_data[di:di+smoothing_window_size,:])
+    train_data[di:di+smoothing_window_size,:] = scaler.transform(train_data[di:di+smoothing_window_size,:])
+
+# You normalize the last bit of remaining data
+scaler.fit(train_data[di+smoothing_window_size:,:])
+train_data[di+smoothing_window_size:,:] = scaler.transform(train_data[di+smoothing_window_size:,:])
+
+# Reshape both train and test data
+train_data = train_data.reshape(-1)
+
+# Normalize test data
+test_data = scaler.transform(test_data).reshape(-1)
