fixed data normalization, added data generator

main.py
@@ -8,11 +8,12 @@ import numpy as np
 # import tensorflow as tf # This code has been tested with TensorFlow 1.6
 from sklearn.preprocessing import MinMaxScaler
 from datareader import DataReader
+from datagenerator import DataGeneratorSeq
 # from datareader import [class name]
 
 # outsourced data-builidng to datareader.py
 
-ticker = 'AAL'
+ticker = 'SPY'
 file_to_save = 'stock_market_data-%s.csv'%ticker
 
 data_reader = DataReader(ticker, file_to_save)
@@ -20,22 +21,23 @@ data_reader.get_data()
 
 df = pd.read_csv(file_to_save)
 
-df = df.sort_values('Date')
+df.sort_values('Date')
 
-high_prices = df.loc[:,'High'].as_matrix()
-low_prices = df.loc[:,'Low'].as_matrix()
+high_prices = df.loc[:,'High'].values
+low_prices = df.loc[:,'Low'].values
 mid_prices = (high_prices+low_prices)/2.0
 
-train_data = mid_prices[:15000]
-test_data = mid_prices[15000:]
+
+train_data = mid_prices[:5400]
+test_data = mid_prices[5400:]
 
 scaler = MinMaxScaler()
 train_data = train_data.reshape(-1,1)
 test_data = test_data.reshape(-1,1)
 
 # Train the Scaler with training data and smooth data
-smoothing_window_size = 2500
-for di in range(0,10000,smoothing_window_size):
+smoothing_window_size = 1250
+for di in range(0,5000,smoothing_window_size):
     scaler.fit(train_data[di:di+smoothing_window_size,:])
     train_data[di:di+smoothing_window_size,:] = scaler.transform(train_data[di:di+smoothing_window_size,:])
 
@@ -53,9 +55,20 @@ test_data = scaler.transform(test_data).reshape(-1)
 # So the data will have a smoother curve than the original ragged data
 EMA = 0.0
 gamma = 0.1
-for ti in range(11000):
+for ti in range(5400):
   EMA = gamma*train_data[ti] + (1-gamma)*EMA
   train_data[ti] = EMA
 
 # Used for visualization and test purposes
 all_mid_data = np.concatenate([train_data,test_data],axis=0)
+
+
+dg = DataGeneratorSeq(train_data,5,5)
+u_data, u_labels = dg.unroll_batches()
+
+for ui,(dat,lbl) in enumerate(zip(u_data,u_labels)):   
+    print('\n\nUnrolled index %d'%ui)
+    dat_ind = dat
+    lbl_ind = lbl
+    print('\tInputs: ',dat )
+    print('\n\tOutput:',lbl)